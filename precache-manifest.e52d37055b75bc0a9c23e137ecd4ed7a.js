98e80b375e7ad67128712a2835e940add5de711c
Andrew Miracle <me@andrewmiracle.com>
2020-10-04 00:10:34 +0000

    Updates

---
+++ precache-manifest.e52d37055b75bc0a9c23e137ecd4ed7a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e56a0e1200989f4987a72f7eee1b538",
+    "url": "./index.html"
+  },
+  {
+    "revision": "888fcd677c2ccc2f322e",
+    "url": "./static/css/main.42cc8b22.chunk.css"
+  },
+  {
+    "revision": "c1bd00d1036d4fed369f",
+    "url": "./static/js/2.6931bb2b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.6931bb2b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "888fcd677c2ccc2f322e",
+    "url": "./static/js/main.745b9489.chunk.js"
+  },
+  {
+    "revision": "097ac53260af17c2e772",
+    "url": "./static/js/runtime-main.f20c76cd.js"
+  }
+]);
